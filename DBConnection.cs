011e2162cadfebd8abb3febfa2e1a7345a0a7578
Jefroxx <matt.oup764@gmail.com>
2025-11-12 10:09:12 +0800

    gi move ra nako ang mga forms into a folder we do need to like make this a bit more organized kay holy hell

---
+++ DBConnection.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalEDPOrderingSystem
+{
+    internal class DBConnection
+    {
+        private static DBConnection instance = null;
+        public String s;
+        private DBConnection()
+        {
+            s = "Data Source=DESKTOP-ABC1234;Initial Catalog=EDPOrderingSystemDB;Integrated Security=True";
+        }
+        public static DBConnection getInstance()
+        {
+            if (instance == null)
+            {
+                instance = new DBConnection();
+            }
+            return instance;
+        }
+    }
+}

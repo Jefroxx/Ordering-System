9ad2cc44f1ad40de043d0a43dd6218fe720bc2ce
Jefroxx <matt.oup764@gmail.com>
2025-11-13 01:30:34 +0800

    Needs Employee Information Table

---
+++ Forms/Admin Side/Add_EditEmployees.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FinalEDPOrderingSystem
+{
+    public partial class Add_EditEmployees : Form
+    {
+        public string Status;
+        public Add_EditEmployees()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAddEmployee_Click(object sender, EventArgs e)
+        {
+            if (!InputCheckers.NullChecker(txtLname, "Last Name")
+             || !InputCheckers.NullChecker(txtFname, "First Name") ||
+             !InputCheckers.NullChecker(txtContactNo, "Contact") ||
+             !InputCheckers.NullChecker(txtAge, "Age") ||
+             !InputCheckers.NullChecker(txtAddress, "Address"))
+                return;
+
+            DBConnection db = DBConnection.getInstance();
+
+            if(Status == "Add")
+            {
+                using (SqlConnection conn = db.GetConnection())
+                {
+                    using (SqlCommand cmd = new SqlCommand("AddEmployee", conn))
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+
+                        cmd.Parameters.AddWithValue("@Last_Name", txtLname.Text.Trim());
+                        cmd.Parameters.AddWithValue("@First_Name", txtFname.Text.Trim());
+                        cmd.Parameters.AddWithValue("@Middle_Initial", txtMI.Text.Trim());
+                        cmd.Parameters.AddWithValue("@Birthday", birthdayPicker.Value.Date);
+                        cmd.Parameters.AddWithValue("@Gender", GenderComboBox.SelectedItem?.ToString());
+                        cmd.Parameters.AddWithValue("@Age", int.Parse(txtAge.Text.Trim()));
+                        cmd.Parameters.AddWithValue("@Contact_Number", txtContactNo.Text.Trim());
+                        cmd.Parameters.AddWithValue("@Address", txtAddress.Text.Trim());
+
+                        conn.Open();
+
+                        int result = Convert.ToInt32(cmd.ExecuteScalar());
+
+                        if (result == 1)
+                        {
+                            MessageBox.Show("Employee " + Status + "ed Successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            this.Close();
+                        }
+                        else
+                        {
+                            MessageBox.Show("An employee with the same first and last name already exists!", "Duplicate", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        }
+                    }
+
+                }
+
+                MessageBox.Show("Employee " + Status + "ed Successfully!");
+                this.Close();
+            }
+            else if (Status == "Edit")
+            {
+                using (SqlConnection conn = db.GetConnection())
+                {
+                    using (SqlCommand cmd = new SqlCommand("AddEmployee", conn))
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+
+                        // Pass parameters
+                        //cmd.Parameters.AddWithValue("@EmployeeID", Convert.ToInt32(txtEmployeeID.Text)); // must have this field
+                        cmd.Parameters.AddWithValue("@Last_Name", txtLname.Text.Trim());
+                        cmd.Parameters.AddWithValue("@First_Name", txtFname.Text.Trim());
+                        cmd.Parameters.AddWithValue("@Middle_Initial", txtMI.Text.Trim());
+                        cmd.Parameters.AddWithValue("@Birthday", birthdayPicker.Value.Date);
+                        cmd.Parameters.AddWithValue("@Contact_Number", txtContactNo.Text.Trim());
+                        cmd.Parameters.AddWithValue("@Gender", GenderComboBox.SelectedItem?.ToString());
+                        cmd.Parameters.AddWithValue("@Age", Convert.ToInt32(txtAge.Text));
+                        cmd.Parameters.AddWithValue("@Address", txtAddress.Text.Trim());
+
+                        conn.Open();
+
+                        int result = Convert.ToInt32(cmd.ExecuteScalar());
+
+                        if (result == 1)
+                        {
+                            MessageBox.Show("Employee updated successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            this.Close();
+                        }
+                        else
+                        {
+                            MessageBox.Show("An employee with the same first and last name already exists!", "Duplicate", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        }
+                    }
+
+                }
+
+            }
+
+        }
+
+        private void Add_EditEmployees_Load(object sender, EventArgs e)
+        {
+            ButtonDesigner.MainButtons(btnAddEmployee);
+            ButtonDesigner.SecondaryButtons(btnCancel);
+            ButtonDesigner.SecondaryButtons(btnUploadImage);
+            ButtonLoader();
+
+        }
+        private void ButtonLoader()
+        {
+            if (Status == "Add")
+            {
+                btnAddEmployee.Text = "Add Employee";
+                btnUploadImage.Text = "Upload Image";
+                btnCancel.Text = "Cancel";
+            }
+            else if (Status == "Edit")
+            {
+                btnAddEmployee.Text = "Save Changes";
+                btnUploadImage.Text = "Change Image";
+                btnCancel.Text = "Discard Changes";
+
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        //FORM CLEANERS
+        private void txtAge_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            InputCheckers.NumbersOnly(e);
+        }
+
+        private void txtContactNo_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            InputCheckers.NumbersOnly(e);
+        }
+
+        private void txtMI_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            InputCheckers.TextOnly(e);
+            e.KeyChar = char.ToUpper(e.KeyChar);
+        }
+
+        private void txtFname_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            InputCheckers.TextOnly(e);
+        }
+
+        private void txtLname_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            InputCheckers.TextOnly(e);
+        }
+
+        private void birthdayPicker_ValueChanged(object sender, EventArgs e)
+        {
+            InputCheckers.ValidatePastOrToday(birthdayPicker, "Birthday");
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ LoginCode/LoginInformation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalEDPOrderingSystem.LoginCode
+{
+    internal class LoginInformation
+    {
+        string Username;
+        string Password;
+
+        public LoginInformation(string username, string password)
+        {
+            this.Username = username;
+            this.Password = password;
+        }
+        public String getUsername()
+        {
+            return this.Username;
+        }
+        public String getPassword()
+        {
+            return this.Password;
+        }
+    }
+}

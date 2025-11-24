6303d16b43a64ad8d05128a8f5d8758585d14563
JustineMantua <My PC@LAPTOP-HUMMEGGV>
2025-11-24 12:43:12 +0800

    a

--- Forms/Admin Side/Add_EditEmployees.Designer.cs
+++ Forms/Admin Side/Add_EditEmployees.Designer.cs
@@ -387,7 +387,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.White;
+            this.BackColor = System.Drawing.Color.Snow;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(1331, 695);
             this.Controls.Add(this.label9);
